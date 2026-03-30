95bc264ad1b8b360d27e15f0a7ea18b89c6c4d48
Himaja192210617 <himajayenikapati@gmail.com>
2026-03-30 11:22:59 +0530

    updated files

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:5000',
+  baseURL: 'http://180.235.121.253:8110',
   headers: {
     'Content-Type': 'application/json',
   },
